a05b0a2f2104452ed9d43e445924887915623aa8
Mateus Oliveira <mateusoliveirap@hotmail.com>
2021-08-02 19:44:12 -0300

    Implementação da cifra

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import KeysView
 import wx
 from scgui import ScGuiMainFrame
 import string
@@ -27,8 +28,33 @@ dicAplha = {}
 for i in list(string.ascii_lowercase):
     number = ord(i) - 97
     dicAplha[number] = i
+dicAplha2 ={}
+for i in list(string.ascii_lowercase):
+    number = ord(i) - 97
+    dicAplha2[i] = number
 
-dicAplha
+
+#teste:
+plainText = 'ATTACK AT DAWN'
+key = cycle('LEMON'.lower())
+plainText = plainText.lower()
+keyStream = ''
+#Gerar o keyStream
+for char in plainText:
+    if char != ' ':
+        keyStream += next(key)
+keyStream = cycle(keyStream)
+    
+#cifrar
+cipherText = ''
+for char in plainText:
+    if char != ' ':
+        cipherText += b[dicAplha2[char]][dicAplha2[next(keyStream)]]
+    else:
+        cipherText += ' '
+print(cipherText)
+
+
 '''
 if __name__ == '__main__':
     # When this module is run (not imported) then create the app, the
